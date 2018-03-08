71c9db95f1ac548c9310c8fa362a3dc11d5859bf
VSoftware <VSoftware@VSoftware-PC>
2018-03-08 13:01:31 +0530

    update

---
+++ src/main/java/com/vs/realestate/entity/Installment.java
@@ -0,0 +1,69 @@
+package com.vs.realestate.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="installment")
+public class Installment {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="mode_name")
+	private String modeName;
+	
+	@Column(name="on_of_installment")
+	private int noOfInstallment;
+	
+	@Column(name="status")
+	private int status;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getModeName() {
+		return modeName;
+	}
+
+	public void setModeName(String modeName) {
+		this.modeName = modeName;
+	}
+
+	public int getNoOfInstallment() {
+		return noOfInstallment;
+	}
+
+	public void setNoOfInstallment(int noOfInstallment) {
+		this.noOfInstallment = noOfInstallment;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Installment [id=" + id + ", modeName=" + modeName + ", noOfInstallment=" + noOfInstallment + ", status="
+				+ status + "]";
+	}
+	
+	
+	
+	
+}
--- src/main/webapp/WEB-INF/view/dashboard.jsp
+++ src/main/webapp/WEB-INF/view/dashboard.jsp
@@ -3,7 +3,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html lang="en">
 <head>
-<title>Matrix Admin</title>
+<title>Vertical</title>
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link rel="stylesheet" href="${pageContext.request.contextPath}/resources/css/bootstrap.min.css"  type='text/css'/>
