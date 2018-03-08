0112d1f98ea65b3453b100481a40224680ee9915
omkar <omkar@omkar>
2018-03-08 11:45:46 +0530

    first commit

---
+++ src/main/java/com/vs/realestate/controller/RealEstateController.java
@@ -0,0 +1,21 @@
+/**
+ *Author : OMKAR SHIVADEKAR
+ *Document : RealEstateController.java
+ *Date : 07-Mar-2018
+ *Time : 5:40:05 PM 
+ */
+package com.vs.realestate.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RealEstateController {
+
+	@RequestMapping("/hello")
+	public String page()
+	{
+		return "dashboard";
+	}
+}
