17e765051e63307f590e409cd2b5327c0dc298eb
omkar <omkar@omkar>
2018-03-09 15:11:45 +0530

    cnflts

--- src/main/java/com/vs/realestate/controller/RealEstateController.java
+++ src/main/java/com/vs/realestate/controller/RealEstateController.java
@@ -6,23 +6,14 @@
  */
 package com.vs.realestate.controller;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.view.RedirectView;
 
 import com.vs.realestate.entity.Organization;
 import com.vs.realestate.service.OrgService;
