8142124d8777582338b1911fc1a4c7958b084366
sarang <sarang@sarang-Inspiron-3521>
2018-04-09 16:32:22 +0530

    changes in sale plot page

--- src/main/java/com/vs/realestate/service/SalePlotService.java
+++ src/main/java/com/vs/realestate/service/SalePlotService.java
@@ -2,6 +2,7 @@ package com.vs.realestate.service;
 
 import java.util.List;
 
+import com.vs.realestate.entity.Installment;
 import com.vs.realestate.entity.Plotting;
 
 public interface SalePlotService {
